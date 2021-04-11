0b62dafb2fe6fec9da0b0d8e39447e5b1c934f6e
Hodacbaoluu <luuhodacbao@gmail.com>
2021-04-11 21:03:40 +0700

    sainuanghihoc

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './components/Header';
+import Carousel from './components/Carousel';
+import Contnet from './components/Contnet';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <div>
+    <Header />
+    <Carousel />
+    <Contnet />
+    <Footer />
+    </div>
+  );
+}
+
+export default App;
